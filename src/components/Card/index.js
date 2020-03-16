2719955bdd1452b8bfb704668523eae9a4143f74
ccostoso <carlos.costoso@outlook.com>
2020-03-16 12:58:32 -0400

    Added topScore funcionality

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,7 +5,10 @@ const Card = props => {
     return (
         <section className="col-3 m-0">
             <article className="card my-1 mx-auto" onClick={props.handleClick}>
-                <img src={props.src} className="card-img-top" alt={props.name}/>
+                <img 
+                    src={props.src} 
+                    className="card-img-top" 
+                    alt={props.name} data-key={props.id}/>
                 {/* <div className="card-body">{props.text}</div> */}
             </article>
         </section>
--- src/components/CardDisplay/index.js
+++ src/components/CardDisplay/index.js
@@ -6,21 +6,45 @@ import cards from './../../cards.json';
 class CardDisplay extends React.Component {
     state = {
         score: 0,
-        topScore: 0
+        topScore: 0,
+        cardArr: [],
+        clickedArr: []
     }
 
-    handleClick = () => {
-        this.setState({
-            score: this.state.score + 1
-        })
+    handleClick = (e) => {
+        const id = e.target.dataset.key;
+        const tempArr = this.state.clickedArr;
+
+        if (tempArr.find(ele => ele === id)) {
+            this.setState({
+                score: 0,
+                clickedArr: []
+            });
+        } else {
+            tempArr.push(id);
+            this.setState({
+                score: this.state.score + 1,
+                topScore: this.state.score === this.state.topScore ? this.state.topScore + 1 : this.state.topScore,
+                clickedArr: tempArr
+            });
+        };
     }
 
     render() {
         return (
             <div>
-                {this.state.score}
+                <p>Score: {this.state.score}</p>
+                <p>Top Score: {this.state.topScore}</p>
+                <p>Clicked: {JSON.stringify(this.state.clickedArr)}</p>
                 <section className="row">
-                    {cards.map(card => <Card src={card.src} name={card.name} key={card.id} handleClick={this.handleClick}/>)}
+                    {cards.map(card => <Card 
+                        src={card.src} 
+                        name={card.name} 
+                        id={card.id}
+                        key={card.id} 
+                        handleClick={this.handleClick} 
+                        isClicked={false} 
+                    />)}
                 </section>
             </div>
         )
